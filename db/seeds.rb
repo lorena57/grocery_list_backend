1babd1b32623be098995f4d5c998811b82472697
github username <github email address>
2020-07-16 14:42:26 -0700

    add faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,13 @@ wholefoods = Market.create(name: "Whole Foods")
 winco = Market.create(name: "Winco")
 
 mondays_list = Grocery.create(grocery_item: "Salt", qty: 2, notes: "Coarse", market_id: 1)
+
+10.times do 
+  Grocery.create({grocery_item: Faker::Food.vegetables, qty: Faker::Number.between(from: 1, to: 10), notes: Faker::Food.description, market_id: 3})
+end
+
+# 10.times do 
+#   Appointment.create({appointment_time: "October 15, 2020 10:48 AM", student_id: 1, tutor_id: 1})
+#   student = Student.create({username: Faker::Internet.username, email: Faker::Internet.unique.email, password: "password"})
+#   tutor = Tutor.create({name: Faker::Name.unique.name, years_exp: "7", SME: Faker::Educator.subject})
+# end
