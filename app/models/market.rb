aaaffa27ce00287ad6fd2d7721fbaac5d253767a
github username <github email address>
2020-07-17 14:07:41 -0700

    create routes

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,5 +1,10 @@
 class Market < ApplicationRecord
 
-    has_many :groceries
+    has_many :groceries, dependent: :destroy
 
 end
+
+# If you set the :dependent option to: :destroy, when the object is destroyed, 
+# destroy will be called on its associated objects. :delete, when the object is 
+# destroyed, all its associated objects will be deleted directly from the database 
+# without calling their destroy method
