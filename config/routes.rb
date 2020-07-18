5760c0585547e189dae833e198a8765bff209459
github username <github email address>
2020-07-18 16:07:43 -0700

    add market index resource to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :groceries, only: [:index, :create]
+      resources :markets, only: [:index]
     end
   end
 
