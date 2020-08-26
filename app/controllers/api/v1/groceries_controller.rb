9b254d2ad0a80c289e22b005a263b6950cb02ce5
github username <github email address>
2020-08-25 21:33:07 -0700

    update controllers

--- app/controllers/api/v1/groceries_controller.rb
+++ app/controllers/api/v1/groceries_controller.rb
@@ -2,50 +2,39 @@ class Api::V1::GroceriesController < ApplicationController
 
     def index
         groceries = Grocery.all
-        render json: groceries
-        # render json: GrocerySerializer.new(groceries)
+        render json: GrocerySerializer.new(groceries)
     end
 
     def create
-        grocery = Grocery.create(grocery_params)
-        grocery.save!
-        render json: grocery
-        
-        # if grocery.save
-        #     render json: grocery, status: :accepted
-        # else
-        #     render json: {errors: grocery.errors.full_messages}
-        # end
+        grocery = Grocery.find_by(id: grocery_params['market_id'])
+        grocery = Grocery.new(grocery_params)
+        grocery.market = market
+        if grocery.save
+            render json: GrocerySerializer.new(grocery), status: :accepted
+        else
+            render json: {errors: grocery.errors.full_messages}
+        end
     end
 
-    # def update
-    #     grocery = Grocery.find(params[:id])
-    #     grocery.update(grocery_params)
-    #     render json: grocery, status: 200    
-    # end
-
-    # def show
-    #     grocery = Grocery.find_by(id: params[:id])
-    #     if grocery 
-    #         render json: GrocerySerializer.new(grocery)
-    #     else 
-    #         render json: {message: 'Groceries not found'}
-            
-    #     end
-    # end
+    def show
+        grocery = Grocery.find_by(id: params[:id])
+        if grocery 
+            render json: GrocerySerializer.new(grocery)
+        else 
+            render json: {message: 'Groceries not found'}
+        end
+    end
 
     def destroy
-        grocery = Grocery.find_by(:id => params[:id]).destroy
-        render json: grocery
+        grocery = Grocery.find_by(id: params[:id])
+        grocery.delete
+        render json: {groceryId: grocery.id}
     end
 
     private
 
     def grocery_params
-        
         params.require(:grocery).permit(:grocery_item, :qty, :notes, :market_id) 
-        
     end
-
 
 end
--- app/controllers/api/v1/markets_controller.rb
+++ app/controllers/api/v1/markets_controller.rb
@@ -3,31 +3,6 @@ class Api::V1::MarketsController < ApplicationController
     def index
         markets = Market.all
         render json: markets
-        # render json: MarketSerializer.new(markets)
     end
-
-    def create
-        if Market.find_by(:name => market_params[:name])
-            market = Market.find_by(:name => market_params[:name])
-        redirect_to '/api/v1/markets/#{market.id}'
-        else
-            market = Market.create(market_params)
-            market.save!
-            render json: market
-    end
-end
-
-
-    def show
-        market = Market.find_by(:id =>params[:id])
-        render json: market
-    end
-
-    private
-
-    def market_params
-        params.require(:market).permit(:name)
-    end
-
-
+    
 end
